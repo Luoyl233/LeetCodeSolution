542e19386120a8ac1d9f0a8e7a73fa14d92efcd3
Luoyl233 <luoyili233@gmail.com>
2019-08-27 22:32:59 +0800

    Add solutions

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,56 @@
+/**
+ * 102. Binary Tree Level Order Traversal
+ */
+
+#include "common.h"
+
+#if LEET_CODE == 102
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == NULL)
+            return ans;
+
+        vector<int> level;
+        int curLevelCnt = 1, nextLevelCnt = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            // This is not a good solution.
+            // We should process a level in a for loop.
+            if (curLevelCnt == 0) {
+                ans.push_back(level);
+                curLevelCnt = nextLevelCnt;
+                nextLevelCnt = 0;
+                level.clear();
+            }
+            TreeNode *p = q.front(); q.pop();
+            curLevelCnt --;
+            level.push_back(p->val);
+            if (p->left) {
+                q.push(p->left);
+                nextLevelCnt ++;
+            }
+            if (p->right) {
+                q.push(p->right);
+                nextLevelCnt ++;
+            }
+        }
+        if (!level.empty()) // Process the last level.
+            ans.push_back(level);
+
+        return ans;
+    }
+};
+
+#endif
